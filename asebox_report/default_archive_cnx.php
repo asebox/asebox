8b0af58fc176233d202694a172704394d6667abe
asebox <ampst.asebox@gmail.com>
2014-06-01 00:45:24 +0200

    Update default_archive_cnx.php

--- asebox_report/default_archive_cnx.php
+++ asebox_report/default_archive_cnx.php
@@ -1,20 +1,12 @@
 <?php
 
-<<<<<<< HEAD
-   $default_archive_server="LIVE-AFS-FAMOO";
-=======
-   $default_archive_server="TEST-AFS-FAMOO";
->>>>>>> 3.1.0
+   $default_archive_server="";
    $default_archive_user="asemon_usr";
    $default_archive_password="asemon_usr";
    $default_archive_db="";
    $default_archive_charset="iso_1";
 
-<<<<<<< HEAD
-   $databaselist_filter="";  // add a filter here to limit list of databases displayed in "Database" field. Ex. : asemo%
-=======
    $databaselist_filter="ASE%";  // add a filter here to limit list of databases displayed in "Database" field. Ex. : asemo%
->>>>>>> 3.1.0
 
    // Set to true if archive user could be changed, false otherwise. It also manages password.
    $allow_change_archive_user=true;
